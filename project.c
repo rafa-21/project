dfb18522771aa7176d34e1e9a5b0758a95375f1f
rafa-21 <71237443+rafa-21@users.noreply.github.com>
2020-09-19 20:09:42 -0300

    first commit

---
+++ project.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("\t\tProjeto de Programação\n\n\n");
+    printf("\t\tAluno: Rafael da Silva Garcia\n\n\n");
+    printf("\t\tSIG-Task: Uma Agenda de Tarefas e Compromissos");
+	return 0;
+}
