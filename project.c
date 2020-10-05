9ebe9f17d6dd1d0a0ac095af87fe8a4bca4e14cb
rafa-21 <71237443+rafa-21@users.noreply.github.com>
2020-10-05 14:40:34 -0300

    tela inicial

--- project.c
+++ project.c
@@ -1,9 +1,26 @@
 #include <stdio.h>
 
-int main()
-{
-    printf("\t\tProjeto de Programação\n\n\n");
-    printf("\t\tAluno: Rafael da Silva Garcia\n\n\n");
-    printf("\t\tSIG-Task: Uma Agenda de Tarefas e Compromissos");
+void menuProjeto(void);
+
+int main(void){
+    menuProjeto();
 	return 0;
 }
+
+void menuProjeto(void){
+    printf("\n");
+    printf("===================================================================\n\n");
+    printf("           Universidade Federal do Rio Grande do Norte         \n");
+    printf("               Centro de Ensino Superior do Seridó              \n");
+    printf("             Departamento de Computação e Tecnologia               \n");
+    printf("                  Disciplina 1106 - Programação                    \n");
+    printf("      Projeto SIG-Task - Uma Agenda de Tarefas e Compromissos         \n");
+    printf("              Developed by Rafael Garcia - @rafa-21                \n\n");
+    printf("===================================================================\n\n");
+    printf("                        DESCRIÇÃO DO PROJETO                  \n\n");
+    printf("===================================================================\n\n");
+    printf("As diversas atribuições que atualmente as pessoas possuem no seu dia a dia exigem, muitas vezes, o registro dos seus compromissos, seja através de agendas analógicas(papel) ou digitais(software e aplicativos). Uma agenda de tarefas é um programa de anotações de tarefas e compromissos do quais o usuário deseja ser lembrado. O presente projeto tem como objetivo desenvolver um programa de computador que realize o agendamento de tarefas, de forma eficiente, fornecendo lembretes aos usuários sobre compromissos agendados.\n\n");
+    printf("===================================================================\n\n");
+
+}
+
