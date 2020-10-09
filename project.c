7302dab97c28512c2ba674f941be36f0bdbaeb1d
rafa-21 <71237443+rafa-21@users.noreply.github.com>
2020-10-09 17:36:10 -0300

    adicionando telas de opções

--- project.c
+++ project.c
@@ -1,13 +1,33 @@
 #include <stdio.h>
 
-void menuProjeto(void);
+void telaProjeto(void);
+void menuPrincipal(void);
+void cadTarefa(void);
+void verComp(void);
+
 
 int main(void){
-    menuProjeto();
+    int opc;
+    telaProjeto();
+    menuPrincipal();
+    printf("Escolha uma opção: ");
+    scanf("%d", &opc);
+    switch (opc){
+        case 1:
+            cadTarefa();
+            break;
+        case 2:
+            verComp();
+            break;
+        default:
+            printf("Escolha uma alternativa válida. ");
+            
+    }
+
 	return 0;
 }
 
-void menuProjeto(void){
+void telaProjeto(void){
     printf("\n");
     printf("===================================================================\n\n");
     printf("           Universidade Federal do Rio Grande do Norte         \n");
@@ -21,6 +41,20 @@ void menuProjeto(void){
     printf("===================================================================\n\n");
     printf("As diversas atribuições que atualmente as pessoas possuem no seu dia a dia exigem, muitas vezes, o registro dos seus compromissos, seja através de agendas analógicas(papel) ou digitais(software e aplicativos). Uma agenda de tarefas é um programa de anotações de tarefas e compromissos do quais o usuário deseja ser lembrado. O presente projeto tem como objetivo desenvolver um programa de computador que realize o agendamento de tarefas, de forma eficiente, fornecendo lembretes aos usuários sobre compromissos agendados.\n\n");
     printf("===================================================================\n\n");
-
 }
 
+void menuPrincipal(void){
+    printf("===================================================================\n\n");
+    printf("                       MENU PRINCIPAL\n\n");
+    printf("                 1 - Agendar Compromissos\n");
+    printf("                   2 - Consultar Agenda\n\n");
+    printf("===================================================================\n\n");
+}
+
+void cadTarefa(void){
+    printf("Tela de cadastro.");
+}
+
+void verComp(void){
+    printf("Tela de comprimissos.");
+}
